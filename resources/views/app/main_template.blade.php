24d38af3695d6438138dedea123d30c9e22b68f5
Luian Alves <luian.alves@gmail.com>
2021-12-07 08:55:28 -0300

    Tentando remover error HTTP após Postagem

--- resources/views/app/main_template.blade.php
+++ resources/views/app/main_template.blade.php
@@ -8,6 +8,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
     <meta name="description" content="">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <meta name="csrf-token" content="{{ csrf_token() }}"> <!-- Ajax para os modals -->
 
     <meta name="author" content="">
